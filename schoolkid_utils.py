233cf73da5b4d839a00b602d3a6ebffd051e9c15
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-08-13 17:00:37 +0300

    Update schoolkid_utils.py

--- schoolkid_utils.py
+++ schoolkid_utils.py
@@ -8,19 +8,20 @@ def get_schoolkid_by_name(schoolkid_name):
         - None, если не найден или найдено несколько
     При этом выводит соответствующие сообщения
     """
-    students = Schoolkid.objects.filter(full_name__icontains=schoolkid_name)
-    
-    if not students.exists():
-        print(f"Учеников с именем '{schoolkid_name}' не найдено")
-        print("   Проверьте правильность ввода имени")
-        return None
-        
-    if students.count() > 1:
-        print(f"Найдено {students.count()} учеников с именем содержащим '{schoolkid_name}':")
-        print("----------------------------------------")
+    try:
+        return Schoolkid.objects.get(full_name__icontains=schoolkid_name)
+    except Schoolkid.DoesNotExist:
+        print(
+            f"Учеников с именем '{schoolkid_name}' не найдено\n"
+            "   Проверьте правильность ввода имени"
+        )
+    except Schoolkid.MultipleObjectsReturned:
+        students = Schoolkid.objects.filter(full_name__icontains=schoolkid_name)
+        print(
+            f"Найдено {students.count()} учеников с именем содержащим '{schoolkid_name}':\n"
+            "----------------------------------------"
+        )
         for i, student in enumerate(students, 1):
             print(f"{i}. {student.full_name} (класс {student.year_of_study}{student.group_letter})")
         print("Пожалуйста, уточните полное имя ученика из списка выше")
-        return None
-        
-    return students.first()
+    return None
